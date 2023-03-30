9ed306d134f1b2b77edb98ebd1d69350de21e796
Agustin Arla <agusa@DESKTOP-0BG6L2K>
2023-03-30 18:44:37 -0300

    Agregar archivos de proyecto.

---
+++ frmDatos.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace EstructuraDatos
+{
+    partial class frmDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDatos));
+            this.pickFoto = new System.Windows.Forms.PictureBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.lblMateria = new System.Windows.Forms.Label();
+            this.lblCarrera = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCarrera = new System.Windows.Forms.TextBox();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.cmdVolver = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pickFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pickFoto
+            // 
+            this.pickFoto.Image = ((System.Drawing.Image)(resources.GetObject("pickFoto.Image")));
+            this.pickFoto.InitialImage = ((System.Drawing.Image)(resources.GetObject("pickFoto.InitialImage")));
+            this.pickFoto.Location = new System.Drawing.Point(40, 34);
+            this.pickFoto.Name = "pickFoto";
+            this.pickFoto.Size = new System.Drawing.Size(159, 177);
+            this.pickFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pickFoto.TabIndex = 0;
+            this.pickFoto.TabStop = false;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(215, 104);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(215, 75);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(26, 13);
+            this.lblDni.TabIndex = 2;
+            this.lblDni.Text = "Dni:";
+            // 
+            // lblMateria
+            // 
+            this.lblMateria.AutoSize = true;
+            this.lblMateria.Location = new System.Drawing.Point(215, 133);
+            this.lblMateria.Name = "lblMateria";
+            this.lblMateria.Size = new System.Drawing.Size(45, 13);
+            this.lblMateria.TabIndex = 3;
+            this.lblMateria.Text = "Materia:";
+            // 
+            // lblCarrera
+            // 
+            this.lblCarrera.AutoSize = true;
+            this.lblCarrera.Location = new System.Drawing.Point(215, 162);
+            this.lblCarrera.Name = "lblCarrera";
+            this.lblCarrera.Size = new System.Drawing.Size(44, 13);
+            this.lblCarrera.TabIndex = 4;
+            this.lblCarrera.Text = "Carrera:";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Enabled = false;
+            this.txtDni.Location = new System.Drawing.Point(268, 75);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(114, 20);
+            this.txtDni.TabIndex = 5;
+            this.txtDni.Text = "45086990";
+            this.txtDni.TextChanged += new System.EventHandler(this.txtDni_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(268, 104);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(114, 20);
+            this.txtNombre.TabIndex = 6;
+            this.txtNombre.Text = "Agustín Arla";
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.Enabled = false;
+            this.txtCarrera.Location = new System.Drawing.Point(268, 159);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(114, 20);
+            this.txtCarrera.TabIndex = 7;
+            this.txtCarrera.Text = "Analista de Sistema";
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Enabled = false;
+            this.txtMateria.Location = new System.Drawing.Point(268, 133);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(114, 20);
+            this.txtMateria.TabIndex = 8;
+            this.txtMateria.Text = "Estructura de Datos";
+            // 
+            // cmdVolver
+            // 
+            this.cmdVolver.Location = new System.Drawing.Point(316, 238);
+            this.cmdVolver.Name = "cmdVolver";
+            this.cmdVolver.Size = new System.Drawing.Size(66, 21);
+            this.cmdVolver.TabIndex = 9;
+            this.cmdVolver.Text = "Volver";
+            this.cmdVolver.UseVisualStyleBackColor = true;
+            this.cmdVolver.Click += new System.EventHandler(this.cmdVolver_Click);
+            // 
+            // frmDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 271);
+            this.Controls.Add(this.cmdVolver);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.txtCarrera);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.lblCarrera);
+            this.Controls.Add(this.lblMateria);
+            this.Controls.Add(this.lblDni);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.pickFoto);
+            this.Name = "frmDatos";
+            this.Text = "Datos del Programador";
+            ((System.ComponentModel.ISupportInitialize)(this.pickFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pickFoto;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Label lblMateria;
+        private System.Windows.Forms.Label lblCarrera;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCarrera;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.Button cmdVolver;
+    }
+}
---
+++ frmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace EstructuraDatos
+{
+    partial class frmEstructuraDatos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menú = new System.Windows.Forms.MenuStrip();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.datosProgramadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estructuraLinealToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estructuraNoLinealToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Menú.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menú
+            // 
+            this.Menú.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaToolStripMenuItem,
+            this.estructuraLinealToolStripMenuItem,
+            this.estructuraNoLinealToolStripMenuItem});
+            this.Menú.Location = new System.Drawing.Point(0, 0);
+            this.Menú.Name = "Menú";
+            this.Menú.Size = new System.Drawing.Size(800, 24);
+            this.Menú.TabIndex = 0;
+            this.Menú.Text = "msMenu";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.datosProgramadorToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.salirToolStripMenuItem});
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // datosProgramadorToolStripMenuItem
+            // 
+            this.datosProgramadorToolStripMenuItem.Name = "datosProgramadorToolStripMenuItem";
+            this.datosProgramadorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.datosProgramadorToolStripMenuItem.Text = "Datos Programador ";
+            this.datosProgramadorToolStripMenuItem.Click += new System.EventHandler(this.datosProgramadorToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir ";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // estructuraLinealToolStripMenuItem
+            // 
+            this.estructuraLinealToolStripMenuItem.Name = "estructuraLinealToolStripMenuItem";
+            this.estructuraLinealToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.estructuraLinealToolStripMenuItem.Text = "Estructura Lineal";
+            // 
+            // estructuraNoLinealToolStripMenuItem
+            // 
+            this.estructuraNoLinealToolStripMenuItem.Name = "estructuraNoLinealToolStripMenuItem";
+            this.estructuraNoLinealToolStripMenuItem.Size = new System.Drawing.Size(125, 20);
+            this.estructuraNoLinealToolStripMenuItem.Text = "Estructura No Lineal";
+            // 
+            // frmEstructuraDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Menú);
+            this.MainMenuStrip = this.Menú;
+            this.Name = "frmEstructuraDatos";
+            this.Text = "Estructura de Datos";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Menú.ResumeLayout(false);
+            this.Menú.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Menú;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem datosProgramadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem estructuraLinealToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estructuraNoLinealToolStripMenuItem;
+    }
+}
+
